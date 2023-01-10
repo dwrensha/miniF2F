36529c3d59b0892292e122fd23b507792ea6b889
David Renshaw <dwrenshaw@gmail.com>
2023-01-10 16:52:09 -0500

    start adding Lean4 things

---
+++ lean4/lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+
+open Lake DSL
+
+package MiniF2F
+
+@[default_target]
+lean_lib MiniF2F
+
+require Mathlib from git "https://github.com/leanprover-community/mathlib4" @ "3ca4b32c460209587497a62d005d3e61d22f0317"
+
